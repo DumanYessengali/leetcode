c2c024752e563cd77efce23e7b4b48ff26deff89
Duman Yessengali <dakirimanko@gmail.com>
2021-09-13 18:31:40 +0600

    ♈: reverseString

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(heightChecker([]int{1, 1, 4, 2, 1, 3}))
+	reverseString([]byte{'a', 'b', 'c', 'd', 'e'})
 }
--- reverseString.go
+++ reverseString.go
@@ -1,11 +1,12 @@
 package main
 
 func reverseString(s []byte) {
-	i, j := 0, len(s)-1
+	reverseByte(s, 0, len(s)-1)
+}
 
-	for i <= j {
+func reverseByte(s []byte, i, j int) {
+	if i <= j {
 		s[i], s[j] = s[j], s[i]
-		i++
-		j--
+		reverseByte(s, i+1, j-1)
 	}
 }
