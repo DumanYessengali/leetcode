00946c688ba37447d93d7ee8483ea80e565d99a0
Duman Yessengali <balmuaast@gmail.com>
2021-10-26 23:32:35 +0600

    : check-if-all-characters-have-equal-number-of-occurrences

--- check-if-all-characters-have-equal-number-of-occurrences.go
+++ check-if-all-characters-have-equal-number-of-occurrences.go
@@ -5,11 +5,13 @@ func areOccurrencesEqual(s string) bool {
 	for i := 0; i < len(s); i++ {
 		m[s[i]]++
 	}
+
 	k := m[s[0]]
 	for i := range m {
 		if k != m[i] {
 			return false
 		}
 	}
+
 	return true
 }
