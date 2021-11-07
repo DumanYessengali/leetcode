8874a84ed530d3b65733820e0cd74fe3d45d9d6e
Duman Yessengali <balmuaast@gmail.com>
2021-11-07 23:02:25 +0600

    🥩: multiply-strings

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(getLucky("iiii", 1))
-	fmt.Println(getLucky("leetcode", 2))
-	fmt.Println(getLucky("zbax", 2))
+	fmt.Println(multiply("123", "456"))
 }
--- multiply-strings.go
+++ multiply-strings.go
@@ -1,22 +1,34 @@
 package main
 
 func multiply(a string, b string) string {
-	if len(a) < len(b) {
-		a, b = b, a
+	length := len(a) + len(b)
+	a, b = reverse([]byte(a)), reverse([]byte(b))
+	reversedResult := make([]int32, length)
+	for index2, v2 := range b {
+		for index1, v1 := range a {
+			currentPosition := index1 + index2
+			carry := reversedResult[currentPosition]
+			var multiplication int32
+			if carry == 0 {
+				multiplication = (v1-'0')*(v2-'0') + carry
+			} else {
+				multiplication = (v1-'0')*(v2-'0') + carry - '0'
+			}
+			reversedResult[currentPosition] = multiplication%10 + '0'
+			reversedResult[currentPosition+1] += multiplication/10 + '0'
+			if reversedResult[currentPosition] > 57 {
+				reversedResult[currentPosition] -= '0'
+			}
+			if reversedResult[currentPosition+1] > 57 {
+				reversedResult[currentPosition+1] -= '0'
+			}
+		}
 	}
-	for len(a) > len(b) {
-		b = b + "0"
+	result := reverse([]byte(string(reversedResult)))
+	for index := range result {
+		if result[index] > '0' {
+			return result[index:]
+		}
 	}
-
-	//arrByteA := []byte(a)
-	//arrByteB := []byte(b)
-	//var arr []int
-	//for i := len(arrByteA) - 1; i >= 0; i-- {
-	//	k:=0
-	//
-	//	for j := len(arrByteB) - 1; j >= 0; j-- {
-	//
-	//	}
-	//}
-	return ""
+	return "0"
 }
