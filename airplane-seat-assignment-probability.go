2a7d79519021337579229844e8c6055f31e95cba
Duman Yessengali <balmuaast@gmail.com>
2021-11-08 18:41:28 +0600

    🥟: airplane-seat-assignment-probability

--- airplane-seat-assignment-probability.go
+++ airplane-seat-assignment-probability.go
@@ -1,5 +1,14 @@
 package main
 
 func nthPersonGetsNthSeat(n int) float64 {
-	return 1/float64(n) + (float64(n)-2)/float64(n)*nthPersonGetsNthSeat(n-1)
+	if n == 1 {
+		return 1
+	}
+
+	prev := 1.0 / float64(n)
+
+	for i := 1; i < n-1; i++ {
+		prev = prev * (1.0 + 1.0/float64(n-i))
+	}
+	return 1 - prev
 }
