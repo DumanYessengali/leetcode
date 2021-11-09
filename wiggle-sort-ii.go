25dc42c97ffcd1423c75c1907022b7d9504bb022
Duman Yessengali <balmuaast@gmail.com>
2021-11-09 18:48:54 +0600

    🥠: wiggle-sort-ii

--- wiggle-sort-ii.go
+++ wiggle-sort-ii.go
@@ -1,25 +1,27 @@
 package main
 
-import "fmt"
-
+// first method
 func wiggleSort(nums []int) {
 	sort(nums)
-	//n1 := nums[:len(nums)-len(nums)/2]
-	//
-	//n2 := nums[len(nums)-len(nums)/2:]
-	//
-	//fmt.Println(n1, n2)
+	n := len(nums)
 
-	k := len(nums) - len(nums)/2
+	arr := make([]int, n, n)
+	i := 1
+	j := n - 1
 
-	for i := 1; i < len(nums); i += 2 {
-		m := k
-		nums[i], nums[k] = nums[k], nums[i]
-		for i+1 < m {
-			nums[m], nums[m-1] = nums[m-1], nums[m]
-			m--
-		}
-		k++
+	for i < n {
+		arr[i] = nums[j]
+		i += 2
+		j--
 	}
-	fmt.Println(nums)
+	i = 0
+	for i < n {
+		arr[i] = nums[j]
+		j--
+		i += 2
+	}
+
+	for index := range nums {
+		nums[index] = arr[index]
+	}
 }
