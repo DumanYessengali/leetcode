9b36c8321367931020a34c3f344521ced6d11d99
DumanYessengali <balmuaast@gmail.com>
2021-11-22 20:48:41 +0600

    🧁: insert-interval ------> do better

---
+++ insert-interval.go
@@ -0,0 +1,30 @@
+package main
+
+// TODO:: better
+func insertInterval(in [][]int, newInterval []int) [][]int {
+	in = append(in, newInterval)
+	if len(in) <= 1 {
+		return in
+	}
+	for i := len(in); i > 0; i-- {
+		for j := 1; j < i; j++ {
+			if in[j][0] < in[j-1][0] {
+				in[j], in[j-1] = in[j-1], in[j]
+			}
+		}
+	}
+	arr := append([][]int{}, in[0])
+	for i := 1; i < len(in); i++ {
+		l := len(arr) - 1
+		if arr[l][1] >= in[i][0] && (arr[l][0] <= in[i][0] || arr[l][1] <= in[i][1] || arr[l][0] <= in[i][1]) {
+			arr[l][1] = max(in[i][1], arr[l][1])
+			arr[l][0] = min(in[i][0], arr[l][0])
+		} else {
+			arr = append(arr, in[i])
+			if arr[l][1] < arr[len(arr)-2][0] {
+				arr[l], arr[len(arr)-2] = arr[len(arr)-2], arr[l]
+			}
+		}
+	}
+	return arr
+}
