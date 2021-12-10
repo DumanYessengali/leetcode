71c49658aa003e4fb0bc1aa5139a787e1a6e9e62
DumanYessengali <balmuaast@gmail.com>
2021-12-10 19:27:06 +0600

    🍷: jump-game

---
+++ jump-game.go
@@ -0,0 +1,12 @@
+package main
+
+func canJump(nums []int) bool {
+	lastPosition := len(nums) - 1
+
+	for i := lastPosition; i >= 0; i-- {
+		if i+nums[i] >= lastPosition {
+			lastPosition = i
+		}
+	}
+	return lastPosition == 0
+}
