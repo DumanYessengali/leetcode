d23ce58ee566a31582b08311f7f9134a6de2d035
DumanYessengali <balmuaast@gmail.com>
2022-05-18 11:44:31 +0600

    🦵: climbing-stairs with recusion

--- climbing-stairs.go
+++ climbing-stairs.go
@@ -13,3 +13,23 @@ func climbStairs(n int) int {
 	}
 	return s
 }
+func climbStairsRecursion(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	return climb(1, 2, n)
+}
+func climb(f, s, n int) int {
+	if n == 1 || n == 2 {
+		return s
+	}
+	if 2 < n {
+		t := f + s
+		f = s
+		s = t
+	}
+	return climb(f, s, n-1)
+}
--- main.go
+++ main.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	root := &TreeNode{Val: 4}
-	BTreeInsertData(root, 2)
-	BTreeInsertData(root, 7)
-	BTreeInsertData(root, 1)
-	BTreeInsertData(root, 3)
-	BTreeApplyInorder(searchBST(root, 2), fmt.Println)
+	fmt.Println(climbStairsRecursion(3))
 }
