28f407b99702e8af160b5a9cc5e5506a498495e9
DumanYessengali <balmuaast@gmail.com>
2022-05-21 12:57:24 +0600

    📋 Find Pivot Index

---
+++ FindPivotIndex.java
@@ -0,0 +1,23 @@
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return pivotIndexRec(sum,0,0,nums);
+    }
+
+    public int pivotIndexRec(int sum, int leftSum, int i, int[] nums) {
+        if (i < nums.length) {
+            sum -= nums[i];
+            if (sum == leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        if (i == nums.length-1){
+            return -1;
+        }
+        return pivotIndexRec(sum, leftSum, ++i, nums);
+    }
+}
